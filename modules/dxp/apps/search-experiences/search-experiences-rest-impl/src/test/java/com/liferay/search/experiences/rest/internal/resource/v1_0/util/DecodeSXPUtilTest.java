82fb6b75dc091b49264de431be98856e9b50d46e
Selena Aungst <selena.aungst@liferay.com>
2026-04-24 02:06:24 -0700

    LPD-28402 search-experiences-rest-impl: Add DecodeSXPUtilTest. https://liferay.atlassian.net/browse/LPD-28402

---
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/test/java/com/liferay/search/experiences/rest/internal/resource/v1_0/util/DecodeSXPUtilTest.java
@@ -0,0 +1,89 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.search.experiences.rest.internal.resource.v1_0.util;
+
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.search.experiences.rest.dto.v1_0.ElementDefinition;
+import com.liferay.search.experiences.rest.dto.v1_0.Field;
+import com.liferay.search.experiences.rest.dto.v1_0.FieldSet;
+import com.liferay.search.experiences.rest.dto.v1_0.SXPElement;
+import com.liferay.search.experiences.rest.dto.v1_0.UiConfiguration;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Selena Aungst
+ */
+public class DecodeSXPUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testDecodeSXPElement() throws Exception {
+		Field field1 = new Field();
+
+		field1.setDefaultValue(
+			Arrays.asList(
+				HashMapBuilder.put(
+					"label", "com.liferay.blogs.model.BlogsEntry"
+				).put(
+					"value", "com.liferay.blogs.model.BlogsEntry"
+				).build(),
+				HashMapBuilder.put(
+					"label", "com.liferay.journal.model.JournalArticle"
+				).put(
+					"value", "com.liferay.journal.model.JournalArticle"
+				).build()));
+		field1.setLabel("Entry Class Names");
+		field1.setName("entry_class_names");
+		field1.setType("multiselect");
+
+		FieldSet fieldSet = new FieldSet();
+
+		fieldSet.setFields(new Field[] {field1});
+
+		UiConfiguration uiConfiguration = new UiConfiguration();
+
+		uiConfiguration.setFieldSets(new FieldSet[] {fieldSet});
+
+		ElementDefinition elementDefinition = new ElementDefinition();
+
+		elementDefinition.setUiConfiguration(uiConfiguration);
+
+		SXPElement sxpElement = new SXPElement();
+
+		sxpElement.setElementDefinition(elementDefinition);
+
+		DecodeSXPUtil.decodeSXPElement(sxpElement);
+
+		FieldSet[] fieldSets = sxpElement.getElementDefinition(
+		).getUiConfiguration(
+		).getFieldSets();
+
+		Assert.assertEquals(Arrays.toString(fieldSets), 1, fieldSets.length);
+
+		Field[] fields = fieldSets[0].getFields();
+
+		Assert.assertEquals(Arrays.toString(fields), 1, fields.length);
+
+		Field field2 = fields[0];
+
+		Assert.assertEquals("entry_class_names", field2.getName());
+		Assert.assertEquals("multiselect", field2.getType());
+
+		Assert.assertNotNull(field2.getDefaultValue());
+	}
+
+}
